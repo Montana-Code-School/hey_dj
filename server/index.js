f7c7d5d8014c6bee4dd3cecc286286951a30e4a3
Thunderbuns <gaberau@yahoo.com>
2017-11-17 13:54:44 -0700

    added warning comment

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const config = require('../config');
-const mongoose = require('mongoose');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const config = require("../config");
+const mongoose = require("mongoose");
 mongoose.connect(config.db);
-const path = require('path');
+const path = require("path");
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
-app.use(express.static('build'));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../build')));
-
-app.listen(config.port)
+app.use(express.static("build"));
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "../build")));
+//not sure if this route should go here
+app.listen(config.port);
