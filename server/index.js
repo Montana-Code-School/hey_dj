c52b40187efe935197453eacd3ecfc69a9933512
Kathryn_Marie_Kelly <kathryn.marie.kelly@gmail.com>
2017-11-17 17:09:19 -0700

    modified server/index and created model - added post route create music set

--- server/index.js
+++ server/index.js
@@ -1,16 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const config = require('../config');
-const mongoose = require('mongoose');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const config = require("../config");
+const mongoose = require("mongoose");
+const musicSet = require("./models/musicSet");
 mongoose.connect(config.db);
-const path = require('path');
+const path = require("path");
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
-app.use(express.static('build'));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../build')));
+app.post("/musicSet", (req, res) => {
+  var newMusicSet = new MusicSet({
+    title: req.body.title,
+    userID: req.body.userID
+  });
+  newMusicSet.save((err, result)  => {
+    if err {
+      response.status(500).json(err)
+    }else {
+      res.json(result);
+    }
+  })
+});
 
-app.listen(config.port)
+app.use(express.static("build"));
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "../build")));
+
+app.listen(config.port);
--- tests/server.test.js
+++ tests/server.test.js
@@ -1,19 +1,20 @@
-import test from 'ava';
-import config from '../config';
-import mongoose from 'mongoose';
-import fakeController from '../server/controllers/fake'
-test.before(()=> {
-    mongoose.connect(config.db)
-})
+import test from "ava";
+import config from "../config";
+import mongoose from "mongoose";
+import fakeController from "../server/controllers/fake";
+test.before(() => {
+  mongoose.connect(config.db);
+});
 
-test("test tests", t=>t.pass())
-
+test("test tests", t => t.pass());
 
 test.cb("example test for", t => {
-  const mockReq = {body: { data: 13}};
-  const mockRes = { json: (data) => {
-  	t.deepEqual(data, {route: 'fake'})
-  	t.end()
-  }}
-  fakeController.fakeRoute(mockReq, mockRes)
+  const mockReq = { body: { data: 13 } };
+  const mockRes = {
+    json: data => {
+      t.deepEqual(data, { route: "fake" });
+      t.end();
+    }
+  };
+  fakeController.fakeRoute(mockReq, mockRes);
 });
