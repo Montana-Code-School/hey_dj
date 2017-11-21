efa3cdd1bcd077b8aee97e2e722cf0f2aa65b397
Kathryn_Marie_Kelly <kathryn.marie.kelly@gmail.com>
2017-11-21 16:39:16 -0700

    createMusicSet is now written async await

--- server/index.js
+++ server/index.js
@@ -4,26 +4,16 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const config = require("../config");
 const mongoose = require("mongoose");
-const musicSet = require("./models/musicSet");
+const musicSet = require("../models/musicSet");
+const { createMusicSet } = require("./controllers/createMusicSet");
 mongoose.connect(config.db);
 const path = require("path");
+const routifyPromise = require("./controllers/util").routifyPromise;
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
-app.post("/musicSet", (req, res) => {
-  var newMusicSet = new MusicSet({
-    title: req.body.title,
-    userID: req.body.userID
-  });
-  newMusicSet.save((err, result)  => {
-    if err {
-      response.status(500).json(err)
-    }else {
-      res.json(result);
-    }
-  })
-});
+app.post("/musicSet", routifyPromise(createMusicSet));
 
 app.use(express.static("build"));
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "../build")));
