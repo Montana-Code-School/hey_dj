884b55d04df3a5a8691e85452701ec75907d3bb0
Thunderbuns <gaberau@yahoo.com>
2017-11-22 08:58:53 -0700

    In this commit we removed all of the commented code and the console logs. currently this code does not work due to objectIds and refs in the schema. Hoping to fix that

--- server/controllers/createPlaylist.js
+++ server/controllers/createPlaylist.js
@@ -1,21 +1,4 @@
-//import playlist from ".../models/playlist.js";
 var playlist = require("../models/playlist.js");
-
-// exports.createPlaylist = function(req, res) {
-//   //construct the main create user function
-//
-//   console.log("req.body is ", req.body);
-//
-//   playlist.create(req.body, function(err) {
-//     if (err) {
-//       console.log(err);
-//       res.json({ success: false });
-//     } else {
-//       console.log("playlist created successfully");
-//       res.json({ success: true });
-//     }
-//   });
-// };
 
 module.exports = {
   createPlaylist: async (req, res) => {
--- server/controllers/util.js
+++ server/controllers/util.js
@@ -1,6 +1,5 @@
 module.exports = {
   routifyPromise: promiseFn => {
-    console.log("This is what your are looking for ", promiseFn);
     return (req, res) => {
       promiseFn(req, res)
         .then(result => res.json(result))
