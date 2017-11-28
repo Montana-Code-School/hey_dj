e9ec8bf03937a6c2fbd429534ba3c5e9fa4ac771
Broc Roberts <brocroberts@Genesis.local>
2017-11-28 15:40:56 -0700

    Added redux base

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { connect } from "react-redux";
+import { increment, decrement } from "./actions/exampleActions";
+import Login from "./Login";
 class App extends Component {
   render() {
     return (
       <div className="App">
+        {this.props.username ? "Hello " + this.props.username : <Login />}
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Counter!!!</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.props.count}
+          <button onClick={this.props.incr}>+1</button>
+          <button onClick={this.props.decr}>-1</button>
         </p>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  username: state.username,
+  count: state.count
+});
+
+const mapDispatchToProps = dispatch => ({
+  incr: () => dispatch(increment()),
+  decr: () => dispatch(decrement())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
