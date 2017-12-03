a7980435b09d6f58c9d0a4109104bf94c380c537
Broc Roberts <brocroberts@Genesis.local>
2017-12-02 17:41:11 -0700

    finished resolving merge issues from yesterday

--- src/App.js
+++ src/App.js
@@ -4,27 +4,19 @@ import "./App.css";
 import { connect } from "react-redux";
 import SpotifyToken from "./components/spotifyTokenComponent/spotifyToken";
 import SpotifyMusicPlaylist from "./components/spotifyMusicPlaylist/spotifyMusicPlaylist";
-
-export default class App extends Component {
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import loginSignUp from "./components/loginSignUp/loginSignUp";
-import dummyUser from "./components/loginSignUp/dummyUser";
 import Error from "./components/error/error";
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <Router>
         <div>
           <Route exact path="/spotifytoken" component={SpotifyToken} />
-          <Route exact path="/" component={SpotifyToken} />
           <Error />
           <Route exact path="/" component={loginSignUp} />
-          <Route path="/dummy" component={dummyUser} />
         </div>
       </Router>
     );
   }
 }
-
-export default App;
